b4b5e6f14bb03afaa682870736639debb534d36e
Esko Janatuinen <esko.janatuinen@gmail.com>
2022-06-22 00:56:47 +0300

    Edit projects

--- pages/portfolio.jsx
+++ pages/portfolio.jsx
@@ -24,7 +24,6 @@ const portfolio = () => {
 
       <div className='max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 pt-8'>
         <div className='col-span-4'>
-          <p>Project</p>
           <h2>Overview</h2>
           <p className='mt-4'>
             Coming soon...
