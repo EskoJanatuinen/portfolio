2279cb1c88e45d188c4bfb57510caf8e6997f2b5
Esko Janatuinen <esko.janatuinen@gmail.com>
2022-06-22 11:30:23 +0300

    Edit text

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -121,7 +121,7 @@ const Navbar = () => {
                     </div>
                 </div>
                 <div className='border-b border-gray-300 my-4'>
-                    <p className='w-[85%] md:w-[90%] py-4'>Let's make digital sales grow</p>
+                    <p className='w-[85%] md:w-[90%] py-4'>Let&#39;s make digital sales grow</p>
                 </div>
                 <div className='py-4 flex flex-col'>
                     <ul className='uppercase'>
