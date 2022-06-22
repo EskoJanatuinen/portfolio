5be0f48c70d31764930a918b497c90d642aed77a
Esko Janatuinen <esko.janatuinen@gmail.com>
2022-06-22 15:23:55 +0300

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,7 @@ module.exports = {
     "rules": {
         "react/react-in-jsx-scope": "off",
         "react/jsx-uses-react": "error",   
-        "react/jsx-uses-vars": "error" 
+        "react/jsx-uses-vars": "error",
+        "react/prop-types": "off"
     }
 }
