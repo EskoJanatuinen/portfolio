0496cf7ec5d5b301ab36d46e7bdf18203b0566b7
Esko Janatuinen <esko.janatuinen@gmail.com>
2022-06-22 11:23:18 +0300

    Fix typo

--- pages/portfolio.jsx
+++ pages/portfolio.jsx
@@ -34,7 +34,7 @@ const portfolio = () => {
           <a
             href='https://github.com/EskoJanatuinen/'
             target='_blank'
-            rrel='noreferrer noopener'
+            rel='noreferrer noopener'
           >
             <button className='px-8 py-2 mt-4'>Code</button>
           </a>
