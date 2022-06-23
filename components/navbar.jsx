dae178247f977aaf5e7ac104515c0329005eb6c2
Esko Janatuinen <esko.janatuinen@gmail.com>
2022-06-23 09:24:21 +0300

    Fix image

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -112,7 +112,7 @@ const Navbar = () => {
         >
             <div>
                 <div className='flex w-full items-center justify-between -my-6'>
-                    <Image src='/../public/assets/Esko_b_540x180.png' width='180' height='60' alt='/' />
+                    <Image src={logo_b} width='180' height='60' alt='/' />
                     <div
                         onClick={handleNav}
                         className='rounded-full shadow-lg shadow-gray-500 p-3 cursor-pointer'
