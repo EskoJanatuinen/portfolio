bca89e9c43ca4b6f7c1737e32b852a44a206eacd
Esko Janatuinen <esko.janatuinen@gmail.com>
2022-06-23 09:07:52 +0300

    testing

--- components/about.jsx
+++ components/about.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link'
-import esko from '../public/assets/Esko_Janatuinen.jpg'
+import esko from '../public/assets/Esko_Janatuinen.png'
 
 const About = () => {
   return (
