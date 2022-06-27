1dc0d016b9a7a8bbd79b4175debe17d9dd115870
Esko Janatuinen <esko.janatuinen@gmail.com>
2022-06-27 23:11:11 +0300

    Change paddings

--- components/project_item.jsx
+++ components/project_item.jsx
@@ -4,11 +4,11 @@ import React from 'react'
 
 const ProjectItem = ({title, backgroundImg, info, projectUrl}) => {
   return (
-    <div className='p-2 md:p-4 relative flex items-center justify-center h-auto w-full shadow-lg shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]'>
+    <div className='p-3 md:p-4 relative flex items-center justify-center h-auto w-full shadow-lg shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]'>
     <Image className='rounded-xl group-hover:opacity-10' src={backgroundImg} alt='/' /> 
     <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
         <h3 className='text-base md:text-2xl text-white tracking-wider text-center'>{title}</h3>
-        <p className='pb-4 pt:1 md:pt-2 text-sm md:text-base text-white text-center'>{info}</p>
+        <p className='pb-4 pt-1 md:pt-2 text-sm md:text-base text-white text-center'>{info}</p>
         <Link href={projectUrl}>
             <p className='text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>Info</p>
         </Link>
