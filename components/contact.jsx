24411274d34fc729e5b66b85f8eb54a2e690116f
Esko Janatuinen <esko.janatuinen@gmail.com>
2022-07-13 00:24:16 +0300

    Chance font size

--- components/contact.jsx
+++ components/contact.jsx
@@ -6,12 +6,12 @@ const Contact = () => {
 
   return (
     <div id='contact' className='w-full lg:h-screen px-6 flex align-middle py-16 md:py-0'>
-      <div className='max-w-[960px] m-auto px-2 w-full'>
+      <div className='max-w-[800px] m-auto px-2 w-full'>
         <p className='text-xl tracking-widest uppercase text-[#5651e5] flex justify-center'>
           Contact
         </p>
         <h2 className='py-4 flex justify-center'>Get In Touch</h2>
-        <p className='p-6 text-gray-600 sm:max-w-[70%] m-auto text-center'>
+        <p className='p-6 text-gray-600 sm:max-w-[70%] m-auto text-center md:text-lg'>
             Are you passionate about creating extraordinary online experiences? I&#39;d love to connect with you and share ideas on how to make digital sales 
             <span className='text-[#5651e5] animate-pulse hover:text-[green] hover:text-2xl hover:uppercase hover:font-extrabold hover:p-4 ease-in duration-500'> grow!</span>
         </p>
