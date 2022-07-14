9d7cf64c8d48b9319969d1b5ae2873956e62f29b
Esko Janatuinen <esko.janatuinen@gmail.com>
2022-07-14 11:11:21 +0300

    Change text size

--- components/footer.jsx
+++ components/footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <div className="sm:py-10 lg:mt-4 lg:pt-4">
             <div className="max-w-6xl mx-12 md:mx-auto pt-8">
                 <div className="pb-4 mb-2 border-t-2"></div>
-                <div className="flex flex-col justify-between lg:flex-row items-center mb-8 md:mb-0">
+                <div className="flex flex-col justify-between lg:flex-row items-center mb-8 md:mb-0 text-sm">
                     <p >Built from scratch with Next.js, Tailwind & Vercel</p>
                 </div>
             </div>
