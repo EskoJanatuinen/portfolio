216e5a76c98a9283b95599f71e5bff058163760b
Esko Janatuinen <esko.janatuinen@gmail.com>
2022-07-20 11:34:44 +0300

    Add about me text

--- components/about.jsx
+++ components/about.jsx
@@ -11,26 +11,28 @@ const About = () => {
           <p className='uppercase text-xl tracking-widest text-[#5651e5]'>
             About
           </p>
-          <h2 className='py-4'>Who I Am</h2>
+          <h2 className='pt-4 pb-8'>Who I Am</h2>
+
           <p className='py-2 text-gray-600'>
-            / / Coming soon...
+            I have spent last 7 years running and developing a unique online store selling 
+            second-hand items and environmental/circular economy services to consumers. 
+            Selling used, one-of-the-kind items online is not a simple mission. Yet, 
+            my team has achieved an average annual sales growth of 65 percent while maintaining profitability.
           </p>
+
           <p className='py-2 text-gray-600'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
-            fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
-            culpa qui officia deserunt mollit anim id est laborum.
+            I have learned through first-hand experience what is essential in digital sales. 
+            From the grass-roots tasks like customer support or picking and packing orders to the concepts 
+            like strategic planning, logistics processes, content creation and technical SEO.
           </p>
+            
           <p className='py-2 text-gray-600'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
-            fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
-            culpa qui officia deserunt mollit anim id est laborum.
+            My background is in economics, but I know my way around technical solutions. In my current role, 
+            I have led several large-scale software development projects. Recent examples include changing our 
+            eCommerce platform from Magento to Nethit Omnisell and building our own customized tool for creating 
+            new products to the product database.
           </p>
+
           <Link href='/#projects'>
             <p className='py-2 text-gray-600 underline cursor-pointer hover:animate-pulse'>
               Check out some of my projects.
