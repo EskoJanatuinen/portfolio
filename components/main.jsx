e6389fd88bc48ba905c32a5dbd4ff447872064bc
Esko Janatuinen <esko.janatuinen@gmail.com>
2022-07-21 16:29:05 +0300

    Test screen sizes

--- components/main.jsx
+++ components/main.jsx
@@ -6,8 +6,8 @@ import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
 const Main = () => {
   return (
     <div id='home' className='w-full h-screen text-center'>
-      <div className='max-w-[1240px] w-full h-screen mx-auto p-2 flex justify-center items-center'>
-        <div>
+      <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
+        <div className='max-h-5/6'>
           <p className='uppercase text-sm tracking-widest text-gray-600'>
             LET&#39;S MAKE DIGITAL SALES GROW
           </p>
