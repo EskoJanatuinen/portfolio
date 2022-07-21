dc8c31d23f64dfc2fb172ce3ea314f3769a7e53c
Esko Janatuinen <esko.janatuinen@gmail.com>
2022-07-21 16:30:22 +0300

    Undo test

--- components/main.jsx
+++ components/main.jsx
@@ -7,7 +7,7 @@ const Main = () => {
   return (
     <div id='home' className='w-full h-screen text-center'>
       <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
-        <div className='max-h-5/6'>
+        <div>
           <p className='uppercase text-sm tracking-widest text-gray-600'>
             LET&#39;S MAKE DIGITAL SALES GROW
           </p>
