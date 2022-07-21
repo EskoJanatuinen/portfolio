2e4f176c14bc467f617e01605324c8026ac5a158
Esko Janatuinen <esko.janatuinen@gmail.com>
2022-07-21 19:16:31 +0300

    Undo test

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,5 +1,4 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
 
 export default class CustomDocument extends Document {
     render() {
@@ -9,7 +8,7 @@ export default class CustomDocument extends Document {
                 <link rel="apple-touch-icon" sizes="180x180" href="/favicon//apple-touch-icon.png" />
                 <link rel="icon" type="image/png" sizes="32x32" href="/favicon//favicon-32x32.png" />
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon//favicon-16x16.png" />
-                <Script>document.querySelector("meta[name=viewport]").setAttribute('content', 'width=device-width, initial-scale='+(1/window.devicePixelRatio));</Script>
+                
             </Head>
             <body>
                 <Main />
