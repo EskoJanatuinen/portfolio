61dc5956ace8a6b008539e7c26d10a37cc4d8bf9
Esko Janatuinen <esko.janatuinen@gmail.com>
2022-07-21 16:31:41 +0300

    Test sizes

--- components/main.jsx
+++ components/main.jsx
@@ -5,7 +5,7 @@ import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
 
 const Main = () => {
   return (
-    <div id='home' className='w-full h-screen text-center'>
+    <div id='home' className='w-full h-screen text-center max-h-[1240px]'>
       <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
         <div>
           <p className='uppercase text-sm tracking-widest text-gray-600'>
