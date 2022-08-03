9eeb748652f9f669294380a15545b5f1f9f7cae1
Esko Janatuinen <esko.janatuinen@gmail.com>
2022-08-04 00:58:51 +0300

    Fix mobile view

--- components/project_item.jsx
+++ components/project_item.jsx
@@ -7,9 +7,9 @@ const ProjectItem = ({title, backgroundImg, projectUrl}) => {
     <div className='p-3 md:p-4 relative flex items-center justify-center h-auto w-full shadow-lg shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]'>
     <Image className='rounded-xl group-hover:opacity-10' src={backgroundImg} alt='/' /> 
     <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
-        <h3 className='-mx-6 py-4 sm:py-6 text-base sm:text-2xl text-white tracking-wider text-center'>{title}</h3>
+        <h3 className='py-4 sm:py-6 text-base sm:text-2xl text-white tracking-wider text-center'>{title}</h3>
         <Link href={projectUrl}>
-            <p className='text-center mx-1 py-1 sm:mx-0 sm:py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer hover:scale-110 ease-in duration-300'>Info</p>
+            <p className='px-4 text-center mx-1 py-1 sm:mx-0 sm:py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer hover:scale-110 ease-in duration-300'>Info</p>
         </Link>
     </div>
  </div>
