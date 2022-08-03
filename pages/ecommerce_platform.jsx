7b289b640463e38aebceccd3ff67a6fbef89c6a2
Esko Janatuinen <esko.janatuinen@gmail.com>
2022-08-03 17:31:27 +0300

    Edit text

--- pages/ecommerce_platform.jsx
+++ pages/ecommerce_platform.jsx
@@ -38,7 +38,7 @@ const ecommerce_platform = () => {
                   Migrating from Magento to Nethit proved to be a challenging process. Just getting the product 
                   data out from the old database and into the new platform was complicated, since we had already 
                   more than 100 000 products at the time. Also, this was in 2020, when the covid pandemic went 
-                  global, and we were forced to close most of our operations. My team was reduced effectively to 
+                  global, and we had to close down most of our operations. My team was reduced effectively to 
                   me, one graphic designer and a copywriter. However, we were able to build the new platform in 
                   about four months. The launch itself was successful, without any significant technical setbacks.
                   </p>
