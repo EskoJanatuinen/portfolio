0a125547bfa5dab51b27149cd06070f3e124153f
Esko Janatuinen <esko.janatuinen@gmail.com>
2022-08-04 00:38:56 +0300

    Fix mobile view

--- components/project_item.jsx
+++ components/project_item.jsx
@@ -9,7 +9,7 @@ const ProjectItem = ({title, backgroundImg, projectUrl}) => {
     <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
         <h3 className='py-4 sm:py-6 text-base md:text-2xl text-white tracking-wider text-center'>{title}</h3>
         <Link href={projectUrl}>
-            <p className='text-center mx-3 py-1 sm:mx-0 sm:py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer hover:scale-110 ease-in duration-300'>Info</p>
+            <p className='text-center mx-2 pt-1 pb-2 sm:mx-0 sm:py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer hover:scale-110 ease-in duration-300'>Info</p>
         </Link>
     </div>
  </div>
