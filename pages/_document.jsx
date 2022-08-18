8792981d4de3aa155accd4cde2a48467ab51fd1c
Esko Janatuinen <esko.janatuinen@gmail.com>
2022-08-18 12:58:24 +0300

    Optimize Google Fonts

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -6,7 +6,7 @@ export default class CustomDocument extends Document {
         return <Html lang="en">
             <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
                 <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;700;800&display=swap" rel="stylesheet" />
                 <link rel='icon' href='/favicon/favicon.ico' />
                 <link rel="apple-touch-icon" sizes="180x180" href="/favicon//apple-touch-icon.png" />
