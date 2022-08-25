0843524c412861a5632b05662f2a933519e00fbf
Esko Janatuinen <esko.janatuinen@gmail.com>
2022-08-25 13:38:58 +0300

    Add link

--- components/contact.jsx
+++ components/contact.jsx
@@ -12,42 +12,40 @@ const Contact = () => {
         </p>
         <h2 className='py-4 flex justify-center'>Get In Touch</h2>
         <p className='p-6 text-gray-600 sm:max-w-[70%] m-auto text-center md:text-lg'>
-            Are you passionate about creating extraordinary online experiences? I&#39;d love to connect with you and share ideas on how to make digital sales 
-            <span className='text-[#5651e5] animate-pulse hover:text-[green] hover:text-2xl hover:uppercase hover:font-extrabold hover:p-4 ease-in duration-500'> grow!</span>
+            Are you passionate about creating extraordinary online experiences? I&#39;d love to connect with you and share ideas on how to make digital sales
+            <span className='text-[#5651e5] animate-pulse hover:text-[green] hover:text-2xl hover:uppercase hover:font-extrabold hover:p-4 ease-in duration-500'> 
+              <a href='https://www.linkedin.com/in/esko-janatuinen/' target='_blank' rel='noreferrer noopener'> grow!</a>
+            </span>
         </p>
-
         <div className='flex items-end justify-center pt-6'>
-                  <a
-                    href='https://www.linkedin.com/in/esko-janatuinen/'
-                    target='_blank'
-                    rel='noreferrer noopener'
-                  >
-                    <div className='rounded-full p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
-                      <FaLinkedinIn />
-                    </div>
-                  </a>
-                  <a
-                    href='https://github.com/EskoJanatuinen'
-                    target='_blank'
-                    rel='noreferrer noopener'
-                  >
-                    <div className='rounded-full p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
-                      <FaGithub />
-                    </div>
-                  </a>
-                  <a
-                    href="mailto:esko.janatuinen@gmail.com"
-                    target='_blank'
-                    rel='noreferrer noopener'
-                  >
-                    <div className='rounded-full p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
-                      <AiOutlineMail />
-                    </div>
-                    </a>
-                </div>
-
-
-        
+          <a
+            href='https://www.linkedin.com/in/esko-janatuinen/'
+            target='_blank'
+            rel='noreferrer noopener'
+          >
+            <div className='rounded-full p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+              <FaLinkedinIn />
+            </div>
+          </a>
+          <a
+            href='https://github.com/EskoJanatuinen'
+            target='_blank'
+            rel='noreferrer noopener'
+          >
+            <div className='rounded-full p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+              <FaGithub />
+            </div>
+          </a>
+          <a
+            href="mailto:esko.janatuinen@gmail.com"
+            target='_blank'
+            rel='noreferrer noopener'
+          >
+            <div className='rounded-full p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+              <AiOutlineMail />
+            </div>
+          </a>
+        </div>
       </div>
     </div>
   );
