74c9858280310a71f55c3f48019922ff2071824c
Esko Janatuinen <esko.janatuinen@gmail.com>
2022-08-25 14:00:31 +0300

    Fix paddings

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -124,7 +124,7 @@ const Navbar = () => {
                             </li>
                         </Link>
                     </ul>
-                    <div className='pt-24'>
+                    <div className='pt-20'>
                         <p className='uppercase tracking-widest text-[#5651e5]'>
                             Let&#39;s Connect
                         </p>
