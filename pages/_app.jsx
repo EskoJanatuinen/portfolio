b8e6fe3fd57f7ddf4bd73e07646eed5ea5f9c52e
Esko Janatuinen <esko.janatuinen@gmail.com>
2022-10-16 20:29:19 +0300

    Add GA4 script

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,19 @@
 import Navbar from '../components/navbar';
 import '../styles/globals.css';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA4_MEASUREMENTID}`} />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA4_MEASUREMENTID}');
+        `}
+      </Script>
       <Navbar />
       <Component {...pageProps} />
     </>
