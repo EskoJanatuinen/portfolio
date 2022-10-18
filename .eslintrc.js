9e8bf2bfe44dc69f124a6578cf095130babcd8c7
Esko Janatuinen <esko.janatuinen@gmail.com>
2022-10-18 23:25:23 +0300

    Edit eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,8 @@ module.exports = {
     "extends": [
         "eslint:recommended", 
         "plugin:react/recommended",
-        "plugin:react/jsx-runtime"
+        "plugin:react/jsx-runtime",
+        "next/core-web-vitals"
     ],
     "parserOptions": {
         "ecmaFeatures": {
